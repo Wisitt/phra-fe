b6ac4f57b3c04317f778f9e4261afbcb2452c231
Wisit <wisit.m@devinnowave.co.th>
2025-01-06 20:29:57 +0700

    a

--- src/features/products/types/art.ts
+++ src/features/products/types/art.ts
@@ -1,14 +1,19 @@
 import type { Author } from '@/types/api'
 
 export interface Art {
-  id: number
-  image: string
-  isVerified: boolean
-  name: string
-  ownedAmount: number
-  maxSupply: number
-  reserveDueDate: Date | null
-  price: number
-  author: Author
-  productTypeId: number
+  id: number;
+  image: string;
+  name: string;
+  ownedAmount: number;
+  maxSupply: number;
+  price: number;
+  reserveDueDate: Date | null;
+  username: string;
+  userRoleId: number;
+  productType: number;
+  isVerified: boolean; // <-- เพิ่ม
+  author: Author;      // <-- เพิ่ม
+  productTypeId: number; // <-- เพิ่ม
 }
+
+
